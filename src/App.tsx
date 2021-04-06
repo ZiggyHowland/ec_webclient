121d5b8f7e45f4cb1b06f9d1a5513f7c3ed4f3a2
ZiggyHowland <sigbjorn@hovland.nu>
2021-04-06 20:55:05 +0200

    Small adjustments to main page

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  {/* Source: https://www.w3schools.com/html/html_layout.asp */}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <h2>CSS Layout Flexbox</h2>
+      <p>In this example, we have created a header, two columns/boxes and a footer. On smaller screens, the columns will stack on top of each other.</p>
+      <p>Resize the browser window to see the responsive effect.</p>
+      <p><strong>Note:</strong> Flexbox is not supported in Internet Explorer 10 and earlier versions.</p>
+
+      <header>
+        <h2>Cities</h2>
       </header>
+
+      <section>
+        <nav>
+          <ul>
+            <li><a href="#">London</a></li>
+            <li><a href="#">Paris</a></li>
+            <li><a href="#">Tokyo</a></li>
+          </ul>
+        </nav>
+        
+        <article>
+          <h1>London</h1>
+          <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+          <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+        </article>
+      </section>
+
+      <footer>
+        <p>Footer</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
