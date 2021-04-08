d42567ff04a3064716539f14bbb6bcc14f2ce3a5
ZiggyHowland <sigbjorn@hovland.nu>
2021-04-08 16:55:13 +0200

    First tries on DNB design

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,11 +1,35 @@
+import {Button} from "@dnb/eufemia";
+
+
 function Header(props: any) {
+
+
     return (
-        <div className="App-header">                        
-            <div className="App-name">      
+
+        <div className="App-header">
+            <Button text="Basic Button" />
+            <Button
+                variant="secondary"
+                text="Secondary Button"
+                icon="chevron_right_medium"
+                size="large"
+            />
+            <Button
+                icon="chevron_right"
+                icon_size="medium"
+                size="large"
+            />
+            <p>I'm not Eufemia</p>
+            <div id="app" className="dnb-core-style">
+                <h1 className="dnb-h--xx-large">I have an Eufemia Style</h1>
+                <p className="dnb-p">👉 Me as well</p>
+            </div>
+            <div className="dnb-core-style">
             
                 <header>
                     <h1>{props.appname}</h1>
                 </header>
+
             </div>            
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@dnb/eufemia/style'
 
 ReactDOM.render(
   <React.StrictMode>
