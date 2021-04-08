88bf20ea5125cbd9e957cbb4baed237dc2752f56
ZiggyHowland <sigbjorn@hovland.nu>
2021-04-08 12:46:20 +0200

    Looking for bugs.

--- src/Tests/TestHello.tsx
+++ src/Tests/TestHello.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import RestClient from '../RestClient';
 
 export default function TestHello() {
-    let [helloMessage, setMessage] = React.useState([]);
+    let [helloMessage, setMessage] = React.useState("Hello unknown");
     // Må se på denne syntaxen
 
     React.useEffect( () => {
         var token = sessionStorage.getItem("admin-token");
-        alert(token);
-        RestClient.helloWorld("Sigbjørn", "sfs")
-            .then(                 
-                message => setMessage(message)             
+        //alert(token);
+        RestClient.helloWorld("Salim", token)
+            .then(                                 
+                result => {
+                    alert(result)               
+            })
+            .catch(
+                e => alert(e)
             )
-    }, [])
+    })
 
     return (
         <div>
