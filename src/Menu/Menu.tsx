44411e074373cc73de315c76ebb8f06b430b8c1a
ZiggyHowland <sigbjorn@hovland.nu>
2021-04-09 15:52:13 +0200

    Added H2 console to menu

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -41,8 +41,11 @@ export default function Menu() {
             <span className="token"><Button className="login" text="Auto logout" onClick={logout}/></span>
             <hr /> 
             <li><Anchor href="http://localhost:8111/swagger-ui/" target="_blank">Backend API</Anchor></li>
+            <li><Anchor href="http://localhost:8111/h2-console" target="_blank">H2 Console</Anchor></li>
             <li><Anchor href="https://github.com/Toendel96/ec_webserver" target="_blank">GitHub: Backend</Anchor></li>
             <li><Anchor href="https://github.com/ZiggyHowland/ec_webclient" target="_blank">GitHub: Frontend</Anchor></li>
+            
+
             
           </ul>
         </nav>  
