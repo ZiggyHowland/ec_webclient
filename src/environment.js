f9f44d30b9ad2caae186753491afff486a21da4a
ZiggyHowland <sigbjorn@hovland.nu>
2021-04-09 08:58:42 +0200

    Moved eufemia components (examples) to a separate page

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,8 @@ export function getGlobalVariables() {
     return {
         appname: "Assignment 2 - SPS",
         date_locale: "no-NO",
-        date_options: { year: 'numeric', month: '2-digit', day: '2-digit' },
+        //date_options: { year: 'numeric', month: '2-digit', day: '2-digit' },
+        date_options: { year: 'numeric', month: 'long', day: 'numeric' },
         rest_base_url: "http://localhost:8111",
     } 
 }
